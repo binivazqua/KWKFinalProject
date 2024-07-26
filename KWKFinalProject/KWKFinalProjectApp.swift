872921bd9ce176c8df422841cd2d9a7ec1330e78
binivazqua <binizavazquez@gmail.com>
2024-07-26 11:00:38 -0600

    Initial Commit

---
+++ KWKFinalProject/KWKFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKFinalProjectApp.swift
+//  KWKFinalProject
+//
+//  Created by Biniza Verónica Vázquez Moreno on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct KWKFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
