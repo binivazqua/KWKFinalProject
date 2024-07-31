95dc59920d964724c3eb7f5368e1525254e2d1c9
binivazqua <binizavazquez@gmail.com>
2024-07-31 15:41:49 -0600

    Quiz page skeleton done - QUIZ PAGE

--- KWKFinalProject/quizzesPage.swift
+++ KWKFinalProject/quizzesPage.swift
@@ -16,52 +16,168 @@ struct quizzesPage: View {
             //.font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/).position(CGPoint(x: 140.0, y: 10.0))
             VStack(alignment: .leading, spacing: 0.0){
                 
-                Text("Dive deeper into yourself!")
+                Text("Dive deeper into yourself!").padding([.leading, .bottom])
                
                 ScrollView {
                     LazyVGrid(columns: [GridItem(.flexible(minimum: 150, maximum: 200)), GridItem(.flexible(minimum: 150, maximum: 200))]){
-                        VStack(alignment: .leading){
-                            Image(systemName: "heart")
-                            Text("Name")
-                            Text("Description")
-                                .font(.caption)
+                        
+                        // +++++ VSTACK ++++++ //
+                        VStack(alignment: .center) {
+                            
+                            Image("mbti_square")
+                            
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150.0, height: 150.0)
+                            .cornerRadius(20)
+                            .padding(.top, 10.0)
+                            
+                            VStack(alignment: .leading){
+                                HStack {
+                                Text("MBTI Personality")
+                                Image(systemName: "heart")
+                                }
+                                    
+                                                
+                                Text("Be yourself and answer honestly.")
+                                 .font(.caption)
+                            }.padding(.horizontal, 10)
+                            
+                        
+                        
+                            Link("Take Quiz", destination: URL(string: "https://www.16personalities.com/free-personality-test")!)
+                                .frame(width: 100.0, height: 20.0)
+                                .padding(10.0)
+                                .font(.subheadline).foregroundColor(.black)
+                                .background(Rectangle().foregroundColor(.green)).cornerRadius(10)
+                            
+                        Spacer()
                         }
-                        .frame(width: 180.0, height: 250.0)
+                          .frame(width: 180.0, height: 300.0)
+                          .background(Rectangle().foregroundColor(.white))
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
                         
-                        .background(Rectangle().foregroundColor(.white)).cornerRadius(20)
-                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
-                        VStack(alignment: .leading){
-                            Image(systemName: "heart")
-                            Text("Name")
-                            Text("Description")
-                                .font(.caption)
+                        // +++++++++ V STACK ++++++++++++ //
+                        
+                        // +++++ VSTACK ++++++ //
+                        VStack(alignment: .center) {
+                            
+                            Image("mbti_square")
+                            
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150.0, height: 150.0)
+                            .cornerRadius(20)
+                            .padding(.top, 10.0)
+                            
+                            VStack(alignment: .leading){
+                                HStack {
+                                Text("MBTI Personality")
+                                Image(systemName: "heart")
+                                }
+                                    
+                                                
+                                Text("Be yourself and answer honestly.")
+                                 .font(.caption)
+                            }.padding(.horizontal, 10)
+                            
+                        
+                        
+                            Link("Take Quiz", destination: URL(string: "https://www.16personalities.com/free-personality-test")!)
+                                .frame(width: 100.0, height: 20.0)
+                                .padding(10.0)
+                                .font(.subheadline).foregroundColor(.black)
+                                .background(Rectangle().foregroundColor(.green)).cornerRadius(10)
+                            
+                        Spacer()
                         }
-                        .frame(width: 180.0, height: 250.0)
+                          .frame(width: 180.0, height: 300.0)
+                          .background(Rectangle().foregroundColor(.white))
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
                         
-                        .background(Rectangle().foregroundColor(.white)).cornerRadius(20)
-                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                        // +++++++++ V STACK ++++++++++++ //
                         
-                        VStack(alignment: .leading){
-                            Image(systemName: "heart")
-                            Text("Name")
-                            Text("Description")
-                                .font(.caption)
+                        // +++++ VSTACK ++++++ //
+                        VStack(alignment: .center) {
+                            
+                            Image("mbti_square")
+                            
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150.0, height: 150.0)
+                            .cornerRadius(20)
+                            .padding(.top, 10.0)
+                            
+                            VStack(alignment: .leading){
+                                HStack {
+                                Text("MBTI Personality")
+                                Image(systemName: "heart")
+                                }
+                                    
+                                                
+                                Text("Be yourself and answer honestly.")
+                                 .font(.caption)
+                            }.padding(.horizontal, 10)
+                            
+                        
+                        
+                            Link("Take Quiz", destination: URL(string: "https://www.16personalities.com/free-personality-test")!)
+                                .frame(width: 100.0, height: 20.0)
+                                .padding(10.0)
+                                .font(.subheadline).foregroundColor(.black)
+                                .background(Rectangle().foregroundColor(.green)).cornerRadius(10)
+                            
+                        Spacer()
                         }
-                        .frame(width: 180.0, height: 250.0)
+                          .frame(width: 180.0, height: 300.0)
+                          .background(Rectangle().foregroundColor(.white))
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
                         
-                        .background(Rectangle().foregroundColor(.white)).cornerRadius(20)
-                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                        // +++++++++ V STACK ++++++++++++ //
                         
-                        VStack(alignment: .leading){
-                            Image(systemName: "heart")
-                            Text("Name")
-                            Text("Description")
-                                .font(.caption)
+                        // +++++ VSTACK ++++++ //
+                        VStack(alignment: .center) {
+                            
+                            Image("mbti_square")
+                            
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150.0, height: 150.0)
+                            .cornerRadius(20)
+                            .padding(.top, 10.0)
+                            
+                            VStack(alignment: .leading){
+                                HStack {
+                                Text("MBTI Personality")
+                                Image(systemName: "heart")
+                                }
+                                    
+                                                
+                                Text("Be yourself and answer honestly.")
+                                 .font(.caption)
+                            }.padding(.horizontal, 10)
+                            
+                        
+                        
+                            Link("Take Quiz", destination: URL(string: "https://www.16personalities.com/free-personality-test")!)
+                                .frame(width: 100.0, height: 20.0)
+                                .padding(10.0)
+                                .font(.subheadline).foregroundColor(.black)
+                                .background(Rectangle().foregroundColor(.green)).cornerRadius(10)
+                            
+                        Spacer()
                         }
-                        .frame(width: 180.0, height: 250.0)
+                          .frame(width: 180.0, height: 300.0)
+                          .background(Rectangle().foregroundColor(.white))
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
                         
-                        .background(Rectangle().foregroundColor(.white)).cornerRadius(20)
-                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                        // +++++++++ V STACK ++++++++++++ //
+                        
+                       
                         
                         
                     }.padding(10)
