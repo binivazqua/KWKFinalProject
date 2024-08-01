d2b89de2b21436759ff167d05fd88fe394499568
binivazqua <binizavazquez@gmail.com>
2024-08-01 15:37:50 -0600

    final user network data done! -- USER NETWORK DATA

--- KWKFinalProject/userNetworkPage.swift
+++ KWKFinalProject/userNetworkPage.swift
@@ -8,68 +8,147 @@
 import SwiftUI
 
 struct UserNetworkPage: View {
+    
+    // COLORS:
+   
+    let offWhite = Color(red: 0.9686274509803922, green: 0.9294117647058824, blue: 0.8862745098039215)
+    let blueGray = Color(red: 0.5176470588235295, green: 0.6470588235294118, blue: 0.615686274509804)
+    let yellow2 = Color(red: 0.9647058823529412, green: 0.7411764705882353, blue: 0.3764705882352941).opacity(0.9)
+    let lightPink = Color(red: 1, green: 0.9333333333333333, blue: 0.9215686274509803)
+    let buttonColor = Color(red: 1, green: 0.35294117647058826, blue: 0.4117647058823529)
+    let coral = Color(red: 0.9490196078431372, green: 0.5176470588235295, blue: 0.5098039215686274)
+    
+       
+    
     @ObservedObject var userProfile: UserProfile
     @State private var navigateToProfile = false
     @State private var infoSaved = false
     
     var body: some View {
         NavigationStack {
-            VStack {
-                VStack(alignment: .center) {
-                    Image("user-line")
-                    Text("Username")
-                }.padding(.vertical, 5)
+            ZStack{
+                lightPink
+                .edgesIgnoringSafeArea(.all)
                 
-                ScrollView {
-                    VStack(alignment: .leading) {
-                        Text("My interests...").font(.title3).padding(.bottom)
+                VStack {
+                    VStack(alignment: .center) {
+                        Image("user-line")
+                            .foregroundColor(lightPink)
+                        Text("Username")
+                    }.padding(.vertical, 10)
+                    
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            Text("My interests...").font(.title3).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/).padding(.bottom)
+                            
+                            Text("My pronouns are:").padding(.bottom)
+                            TextField("Type...", text: $userProfile.pronouns)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("Age:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.age)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("Word that describes me:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.descriptiveWord)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("I love to:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.passions)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("I am good at:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.talents)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("MBTI Personality Type:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.mbti)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("Mission:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.mission)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                            
+                            Text("Vision:").padding(.vertical)
+                            TextField("Type...", text: $userProfile.vision)
+                                .padding()
+                                .background(blueGray.opacity(0.6))
+                                .cornerRadius(8)
+                                .shadow(radius: 5)
+                                .frame(maxWidth: 300)
+                                .frame(height: 40)
+                        }.padding(10.0)
+                    }
+                    
+                    HStack(alignment: .center, spacing: 10.0) {
+                        Button("Edit", systemImage: "pencil") {
+                            infoSaved = true
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(buttonColor)
                         
-                        Text("My pronouns are:")
-                        TextField("Type...", text: $userProfile.pronouns)
-                        
-                        Text("Age:")
-                        TextField("Type...", text: $userProfile.age)
-                        
-                        Text("Word that describes me:")
-                        TextField("Type...", text: $userProfile.descriptiveWord)
-                        
-                        Text("I love to:")
-                        TextField("Type...", text: $userProfile.passions)
-                        
-                        Text("I am good at:")
-                        TextField("Type...", text: $userProfile.talents)
-                        
-                        Text("MBTI Personality Type:")
-                        TextField("Type...", text: $userProfile.mbti)
-                        
-                        Text("Mission:")
-                        TextField("Type...", text: $userProfile.mission)
-                        
-                        Text("Vision:")
-                        TextField("Type...", text: $userProfile.vision)
-                    }.padding(.leading, 20.0)
+                        // NavigationLink outside the alert
+                        NavigationLink(destination: NetworkPage(userProfile: userProfile), isActive: $navigateToProfile) {
+                            EmptyView()
+                        }
+                        .alert("✅", isPresented: $infoSaved, actions: {
+                               Button("Go to profile") {
+                                     navigateToProfile = false
+                               }
+                            }, message: {
+                                Text("Info saved successfully!")
+                            })
+                    
+                        Button("View Profile", systemImage: "person.fill.viewfinder") {
+                            navigateToProfile = true
+                        }
+                        .buttonStyle(.bordered)
+                        .tint(buttonColor)
+                        .padding(.leading, 20.0)
+                    }.padding()
                 }
+                    
+                   
                 
-                Button("Save info") {
-                        infoSaved = true
-                }
-                .buttonStyle(.bordered)
-                .tint(.green)
-                                
-            // NavigationLink outside the alert
-                NavigationLink(destination: NetworkPage(userProfile: userProfile), isActive: $navigateToProfile) {
-                     EmptyView()
-                }
-               }
-               .alert("✅", isPresented: $infoSaved, actions: {
-                       Button("Go to profile") {
-                             navigateToProfile = true
-                       }
-                    }, message: {
-                        Text("Info saved successfully!")
-                    })
-        }
-        .padding(10)
+            }
+                //.padding(10)
+            }
+            
     }
 }
 
